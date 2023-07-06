d2f79f0ebd747deddd100da346ae51b46a581610
JaydeepBodar <jaydeepbodar732@gmail.com>
2023-07-06 17:44:04 +0530

    url-changes

--- src/backend/db.js
+++ src/backend/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const db = async() => {
     try{
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL2)
         console.log("Connection successfully")
     }catch(e){
         console.log("faield to connect to database")
