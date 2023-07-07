8188f81cc10f200fb8fc2c6a7b601f56adc4f2ed
JaydeepBodar <jaydeepbodar732@gmail.com>
2023-07-07 15:45:50 +0530

    f

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,7 @@ import { env } from "@/config/env";
 //   return data.json();
 // }
 export default function Home() {
+  console.log("first")
   const [product, setproduct] = useState([]);
   useEffect(() => {
     axios
