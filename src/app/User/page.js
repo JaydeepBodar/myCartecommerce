5a04a53b2f18dbfc7327ffa0973cb591e16fe20f
JaydeepBodar <jaydeepbodar732@gmail.com>
2023-07-26 12:26:40 +0530

    cookies

--- src/app/User/page.js
+++ src/app/User/page.js
@@ -5,10 +5,9 @@ async function getData() {
   const nextCookies = cookies();
   const productionheaders = nextCookies.get("__Secure-next-auth.session-token");
   const nextauthheaders = nextCookies.get("next-auth.session-token");
-  const cookie=  process.env.API_URL === "production"
+  const cookie=  process.env.API_URL === "https://my-cartecommerce.vercel.app/"
   ? `__Secure-next-auth.session-token=${productionheaders?.value}`
   : `next-auth.session-token=${nextauthheaders?.value}`
-  console.log("cookie",cookie)
   // console.log("nextauthheaders",nextauthheaders)
   const { data } = await axios.get(`${process.env.API_URL}api/Address`, {
     cache: "no-store",
@@ -24,6 +23,7 @@ async function getData() {
   return data.address;
 }
 const User = async () => {
+  // console.log("cookie",cookie)
   console.log("process.env.API_URL",process.env.API_URL)
   const address = await getData();
   // console.log("address", address);
