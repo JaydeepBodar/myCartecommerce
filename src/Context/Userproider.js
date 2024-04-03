5b8e0f8798e2d35cdc1fe5d5a4c53e0fe22e505c
JaydeepBodar <jaydeepbodar732@gmail.com>
2024-04-03 16:09:28 +0530

    structure_functional_check

--- src/Context/Userproider.js
+++ src/Context/Userproider.js
@@ -20,7 +20,7 @@ const Usercontextprovider = ({ children }) => {
   useEffect(() => {
     loaduser()
   }, [user?.updatedAt])
-  // console.log("user",user)
+  console.log("user")
   return (
     <Usercontext.Provider value={{user ,loading,setuser,loaduser}}>
       {children}
