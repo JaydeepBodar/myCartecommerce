7719dc71652a80faa12604173250c1e3d215f95f
Jaydeep-Bodar <jaydeep.bodar@iflair.com>
2024-08-12 18:18:27 +0530

    small_changes

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { withAuth } from "next-auth/middleware";
 export default withAuth(
   async function middleware(req) {
-    console.log("req.nextauth.token", req.nextauth.token);
+    // console.log("req.nextauth.token", req.nextauth.token);
   }
   // {
   //   callbacks: {
