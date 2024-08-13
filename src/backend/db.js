8dadbef57ab55e1b5d19bfede56a64309fd00995
Jaydeep-Bodar <jaydeep.bodar@iflair.com>
2024-08-13 11:21:15 +0530

    database_check_for_aws

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const db = async () => {
-  try {
+  // try {
     await mongoose.connect(process.env.MONGO_URL2);
     console.log("Connection successfully");
-  } catch (e) {
-    console.log("faield to connect to database");
-  }
+  // } catch (e) {
+    // console.log("faield to connect to database");
+  // }
 };
 
 export default db;
