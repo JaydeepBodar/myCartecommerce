7287c6c781fc9478e972a8ed86c89d5d2bf925e1
Jaydeep-Bodar <jaydeep.bodar@iflair.com>
2024-08-13 11:46:19 +0530

    change_data_base url

--- src/backend/db.js
+++ src/backend/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const db = async () => {
   // try {
+    console.log("process.env.MONGO_URL2)process.env.MONGO_URL2)process.env.MONGO_URL2)",process.env.MONGO_URL2)
     await mongoose.connect(process.env.MONGO_URL2);
     console.log("Connection successfully");
   // } catch (e) {
