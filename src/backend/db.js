9f54bfdfe3654914ee4eed28cbca89fa580215c9
Jaydeep-Bodar <jaydeep.bodar@iflair.com>
2024-08-13 13:03:14 +0530

    db_changes

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 const db = async () => {
-  try {
+  // try {
+    console.log("process.env.MONGO_URL2)process.env.MONGO_URL2)process.env.MONGO_URL2)",process.env.MONGO_URL2)
     await mongoose.connect(process.env.MONGO_URL2);
     console.log("Connection successfully");
-  } catch (e) {
-    console.log("faield to connect to database");
-  }
+  // } catch (e) {
+    // console.log("faield to connect to database");
+  // }
 };
 
 export default db;
